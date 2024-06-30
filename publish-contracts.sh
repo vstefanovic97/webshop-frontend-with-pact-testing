cddd5e1a5683e4f46ff087abfd264c02c80c5069
vstefanovic97 <stefanoviv@ae.com>
2024-06-30 12:22:47 +0200

    Update publish-contract bash script

--- publish-contracts.sh
+++ publish-contracts.sh
@@ -1,13 +1,12 @@
 docker run --rm \
  -w ${PWD} \
  -v ${PWD}:${PWD} \
- -e PACT_BROKER_BASE_URL=http://localhost:3002 \
+ -e PACT_BROKER_BASE_URL=http://localhost:31000 \
  -e PACT_BROKER_USERNAME=vukstefanovic97 \
  -e PACT_BROKER_PASSWORD=test1234 \
  --network="host" \
   pactfoundation/pact-cli:1 \
   publish \
   ${PWD}/pacts \
-  --broker-base-url="http://host.docker.internal:3002" \
   --consumer-app-version fake-git-sha-for-demo-$(date +%s) \
   --tag-with-git-branch
